444fd2f57a5043753a0c72f15d00116b511434dc
atziryflores5627-boop <atziry.flores5627@alumnos.udg.mx>
2026-03-04 22:56:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -158,117 +158,133 @@ def gb_a_mb(gb):
 # Evidencia/ Integración (Estudiante 01 Flores Rentería Atziry) 
 def programa_principal
     lista_historial = cargar_historial()
-                   while True: 
-                       print("-----------------------------------------")
-                       print("CALCULADORA MULTIFUNCIONAL")
-                       print("Equipo: Marco Antonio Reyes Cuevas, Rubén Sánchez Suárez, Atziry Flores Rentería")
-                       print("-----------------------------------------")\n
-                       
-                       print("1. Calculadora básica")
-                       print("2. Conversor de unidades de datos")
-                       print("3. Calculadora de sistemas numéricos")
-                       print("4. Ver historial")
-                       print("5. Limpiar historial")
-                       print("6. Salir") 
-
-                       opcion = input("\nSeleccione una opción: ")
+    while True: 
+    print("-----------------------------------------")
+    print("CALCULADORA MULTIFUNCIONAL")
+    print("Equipo: Marco Antonio Reyes Cuevas, Rubén Sánchez Suárez, Atziry Flores Rentería")
+    print("-----------------------------------------")\n                   
+    print("1. Calculadora básica")
+    print("2. Conversor de unidades de datos")
+    print("3. Calculadora de sistemas numéricos")
+    print("4. Ver historial")
+    print("5. Limpiar historial")
+    print("6. Salir") 
+    opcion = input("\nSeleccione una opción: ")
 #Funcionamiento de las opciones del menú principal "Calculadora" 
 if opcion == "1": 
     print("\n--- CALCULADORA BÁSICA ---")
     print("1. Suma | 2. Resta | 3. Mult. | 4. Div. | 5. Mod | 6. pot") 
     sub_op = input("Seleccione su operación: ")
     n1 = validar_numero("Ingrese su primer número: ")
-    n2 = validar_numero(Ingrese su segundo numero: ")
+    n2 = validar_numero("Ingrese su segundo numero: ")
     res = None
     simbolo = ""
-    if sub_op == "1": res = sumar(n1, n2); simbolo = "+"
-elif sub_op == "2": res = restar(n1, n2); simbolo = "-"
-elif sub_op == "3" res = multiplicar(n1, n2); simbolo = "*"
-elif sub_op == "4" res = dividir(n1, n2); simbolo = "/"
-elif sub_op == "5" res = modulo(n1, n2); simbolo = "%"
-elif sub_op == "6" res = potencia(n1, n2); simbolo = "^"
-
-if res is None: 
-    print("Error: Operación inválida o división por cero."
-          else: 
-    texto = f"{n1} {simbolo} {n2} = {res}"
-    print(f"Resultado: {texto}")
+            if sub_op == "1":
+                res = sumar(n1, n2)
+                simbolo = "+"
+            elif sub_op == "2":
+                res = restar(n1, n2)
+                simbolo = "-"
+            elif sub_op == "3":
+                res = multiplicar(n1, n2)
+                simbolo = "*"
+            elif sub_op == "4":
+                res = dividir(n1, n2)
+                simbolo = "/"
+            elif sub_op == "5":
+                res = modulo(n1, n2)
+                simbolo = "%"
+            elif sub_op == "6":
+                res = potencia(n1, n2)
+                simbolo = "^"
+            if res is None:
+                print("Error: Operación inválida o división por cero.")
+            else:
+                texto = f"{n1} {simbolo} {n2} = {res}"
+                print(f"Resultado: {texto}")
 
 # Funcionamiento de las opciones del menú principal "Unidades"
-elif opcion == "2": 
-print("\n--- CONVERSOR DE DATOS ---")
-print("1. B a KB | 2. KB a MB | 3. MB a GB | 4. KB a B | 5. MB a KB | 6. GB a MB")
-sub_op = input("Seleccione: ")
-val = validar_numero("Cantidad: ")
-
-res = 0 
-txt = ""
-
-if sub_op == "1": res= bytes_a_kb(val); txt = f"{val} Bytes a KB"
-elif sub_op == "2": res= kb_a_mb(val); txt = f"{val} KB a MB"
-elif sub_op == "3": res= mb_a_gb(val); txt = f"{val} MB a GB"
-elif sub_op == "4": res= kb_a_bytes(val); txt = f"{val} KB a Bytes"
-elif sub_op == "5": res= mb_a_kb(val); txt = f"{val} MB a KB"
-elif sub_op == "6": res= gb_a_mb(val); txt = f"{val} GB a MB"
-    print(f"Resultado: {res}")
-lista_historial.append(f"{txt} = {res}")
-
+       elif opcion == "2":
+            print("\n--- CONVERSOR DE DATOS ---")
+            print("1. B a KB | 2. KB a MB | 3. MB a GB | 4. KB a B | 5. MB a KB | 6. GB a MB")
+            sub_op = input("Seleccione: ")
+            val = validar_numero("Cantidad: ")
+            res = 0
+            txt = ""
+            if sub_op == "1":
+                res = bytes_a_kb(val)
+                txt = f"{val} Bytes a KB"
+            elif sub_op == "2":
+                res = kb_a_mb(val)
+                txt = f"{val} KB a MB"
+            elif sub_op == "3":
+                res = mb_a_gb(val)
+                txt = f"{val} MB a GB"
+            elif sub_op == "4":
+                res = kb_a_bytes(val)
+                txt = f"{val} KB a Bytes"
+            elif sub_op == "5":
+                res = mb_a_kb(val)
+                txt = f"{val} MB a KB"
+            elif sub_op == "6":
+                res = gb_a_mb(val)
+                txt = f"{val} GB a MB"
+            print(f"Resultado: {res}")
+            lista_historial.append(f"{txt} = {res}")
 #Funcionamiento de las opciones del menú principal "Sistemas"
-elif opcion == "3"
-print("\n--- SISTEMAS NUMÉRICOS ---")
-print("1. Dec a Bin | 2. Dec a Hex | 3. Bin a Dec | 4. Hex a Dec")
-sub_op = input("seleccione: ")
-
-res = None
-if sub_op == "1":
-    n = validar_numero("Número decimal: ")
-    res = dec2bin(n)
-    lista_historial.append(f"Decimal {n} a Hexadecimal = {res}")
-elif sub_op == "2":
-    n = validar_numero("Número decimal: ")
-    res = dec2hex(n)
-    lista_historial.append(f"Decimal {n} a Hexadecimal = {res}")
-elif sub_op == "3":
-    b = input("Número binario: ")
-    res = bin2dec(b)
-    lista_historial.append(f"Binario {b} a Decimal = {res}")
-elif sub_op == "4":
-    h = input("Número hexadecimal: ")
-    res = hex2dec(h)
-    lista_historial.append(f"Hexadecimal {h} a Decimal = {res}")
-            
-if res is not None:
-    print(f"Resultado: {res}")
-else:
-    print("Error: Entrada de sistema numérico inválida.")
+        elif opcion == "3":
+            print("\n--- SISTEMAS NUMÉRICOS ---")
+            print("1. Dec a Bin | 2. Dec a Hex | 3. Bin a Dec | 4. Hex a Dec")
+            sub_op = input("Seleccione: ")
+            res = None
+            if sub_op == "1":
+                n = validar_numero("Número decimal: ")
+                res = dec2bin(n)
+                lista_historial.append(f"Decimal {n} a Binario = {res}")
+            elif sub_op == "2":
+                n = validar_numero("Número decimal: ")
+                res = dec2hex(n)
+                lista_historial.append(f"Decimal {n} a Hexadecimal = {res}")
+            elif sub_op == "3":
+                b = input("Número binario: ")
+                res = bin2dec(b)
+                lista_historial.append(f"Binario {b} a Decimal = {res}")
+            elif sub_op == "4":
+                h = input("Número hexadecimal: ")
+                res = hex2dec(h)
+                lista_historial.append(f"Hexadecimal {h} a Decimal = {res}")
+            if res is not None:
+                print(f"Resultado: {res}")
+            else:
+                print("Error: Entrada de sistema numérico inválida.")
 
 # Desarrollo de historial
-elif opcion == "4": 
-print("\n" + "-"*10 + " HISTORIAL (Últimas 10) " + "-"*10)
-if not lista_historial:
-    print("No hay registros.")
-else:
-    for item in lista_historial[-10:]:
-# Opción de limpiar historial
-elif opcion == "5":
-    lista_historial.clear()
-    print("HIstorial borrado.")
-# Opción de salir 
-
-elif opcion == "6": 
-guardarhistorial(lista_historial)
-print("¡Graciass por utilizar nuestra calculadora! Tu historial fue guardado.")
-break
-
-else: print("Opción no válida.")
-
+        elif opcion == "4":
+            print("\n" + "-"*10 + " HISTORIAL (Últimas 10) " + "-"*10)
+            if not lista_historial:
+                print("No hay registros.")
+            else:
+                for item in lista_historial[-10:]:
+                    print(item)
+        # Opción de limpiar historial
+        elif opcion == "5":
+            lista_historial.clear()
+            print("Historial borrado.")
+        # Opción de salir
+        elif opcion == "6":
+            guardarhistorial(lista_historial)
+            print("¡Gracias por utilizar nuestra calculadora! Tu historial fue guardado.")
+            break
+        else:
+            print("Opción no válida.")
 # Inicio de programa
-if__if __name__ == "__main__":
-    programa_principal()    
+if __name__ == "__main__":
+    programa_principal()  
 
 
 
                       
+
 
 
 
