955ef0276c2c3715132f21ae6c76b4a156d69d3d
atziryflores5627-boop <atziry.flores5627@alumnos.udg.mx>
2026-03-04 22:11:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -121,6 +121,65 @@ def gb_a_mb(gb):
     return resultado
 
 # Evidencia/ Integración (Estudiante 01 Flores Rentería Atziry) 
+def programa_principal
+    lista_historial = cargar_historial()
+                   while True: 
+                       print("-----------------------------------------")
+                       print("CALCULADORA MULTIFUNCIONAL")
+                       print("Equipo: Marco Antonio Reyes Cuevas, Rubén Sánchez Suárez, Atziry Flores Rentería")
+                       print("-----------------------------------------")\n
+                       
+                       print("1. Calculadora básica")
+                       print("2. Conversor de unidades de datos")
+                       print("3. Calculadora de sistemas numéricos")
+                       print("4. Ver historial")
+                       print("5. Limpiar historial")
+                       print("6. Salir") 
 
-# Yo junto lo que ustedes hagan, para realizar el menú.
+                       opcion = input("\nSeleccione una opción: ")
+#Funcionamiento de las opciones del menú principal "Calculadora" 
+if opcion == "1": 
+    print("\n--- CALCULADORA BÁSICA ---")
+    print("1. Suma | 2. Resta | 3. Mult. | 4. Div. | 5. Mod | 6. pot") 
+    sub_op = input("Seleccione su operación: ")
+    n1 = validar_numero("Ingrese su primer número: ")
+    n2 = validar_numero(Ingrese su segundo numero: ")
+    res = None
+    simbolo = ""
+    if sub_op == "1": res = sumar(n1, n2); simbolo = "+"
+elif sub_op == "2": res = restar(n1, n2); simbolo = "-"
+elif sub_op == "3" res = multiplicar(n1, n2); simbolo = "*"
+elif sub_op == "4" res = dividir(n1, n2); simbolo = "/"
+elif sub_op == "5" res = modulo(n1, n2); simbolo = "%"
+elif sub_op == "6" res = potencia(n1, n2); simbolo = "^"
+
+if res is None: 
+    print("Error: Operación inválida o división por cero."
+          else: 
+    texto = f"{n1} {simbolo} {n2} = {res}"
+    print(f"Resultado: {texto}")
+
+# Funcionamiento de las opciones del menú principal "Unidades"
+elif opcion == "2": 
+print("\n--- CONVERSOR DE DATOS ---")
+print("1. B a KB | 2. KB a MB | 3. MB a GB | 4. KB a B | 5. MB a KB | 6. GB a MB")
+sub_op = input("Seleccione: ")
+val = validar_numero("Cantidad: ")
+
+res = 0 
+txt = ""
+
+if sub_op == "1": res= bytes_a_kb(val); txt = f"{val} Bytes a KB"
+elif sub_op == "2": res= kb_a_mb(val); txt = f"{val} KB a MB"
+elif sub_op == "3": res= mb_a_gb(val); txt = f"{val} MB a GB"
+elif sub_op == "4": res= kb_a_bytes(val); txt = f"{val} KB a Bytes"
+elif sub_op == "5": res= mb_a_kb(val); txt = f"{val} MB a KB"
+elif sub_op == "6": res= gb_a_mb(val); txt = f"{val} GB a MB"
+    print(f"Resultado: {res}")
+lista_historial.append(f"{txt} = {res}")
+
+
+
+
+                      
 
